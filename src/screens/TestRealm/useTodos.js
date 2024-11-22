3d94677ffd4564f312055a3c29774dc612f5d696
enkhturBaina <eegii.111.gg@gmail.com>
2024-11-22 17:47:31 +0800

    zaswar

--- src/screens/TestRealm/useTodos.js
+++ src/screens/TestRealm/useTodos.js
@@ -1,49 +1,49 @@
-import * as SQLite from "expo-sqlite";
-import { useState } from "react";
+// import * as SQLite from "expo-sqlite";
+// import { useState } from "react";
 
-export function useTodos() {
-	const [todos, setTodos] = useState([]);
+// export function useTodos() {
+// 	const [todos, setTodos] = useState([]);
 
-	/**
-	 * Whenever the todos table has mutated, we need to fetch the data set again order to sync DB -> UI State
-	 */
-	const fetchTodos = (tx) => {
-		tx.executeSql("SELECT * FROM todos;", [], (_, { rows: { _array } }) => setTodos(_array));
-	};
+// 	/**
+// 	 * Whenever the todos table has mutated, we need to fetch the data set again order to sync DB -> UI State
+// 	 */
+// 	const fetchTodos = (tx) => {
+// 		tx.executeSql("SELECT * FROM todos;", [], (_, { rows: { _array } }) => setTodos(_array));
+// 	};
 
-	const getTodos = (db) => {
-		db.readTransaction(fetchTodos);
-	};
+// 	const getTodos = (db) => {
+// 		db.readTransaction(fetchTodos);
+// 	};
 
-	const addTodo = (db, title) => {
-		db.transaction((tx) => {
-			tx.executeSql("INSERT INTO todos (title) VALUES (?);", [title]);
+// 	const addTodo = (db, title) => {
+// 		db.transaction((tx) => {
+// 			tx.executeSql("INSERT INTO todos (title) VALUES (?);", [title]);
 
-			fetchTodos(tx);
-		});
-	};
+// 			fetchTodos(tx);
+// 		});
+// 	};
 
-	const updateTodo = (db, id, title) => {
-		db.transaction((tx) => {
-			tx.executeSql("UPDATE todos SET title = ? WHERE id = ?;", [title, id]);
+// 	const updateTodo = (db, id, title) => {
+// 		db.transaction((tx) => {
+// 			tx.executeSql("UPDATE todos SET title = ? WHERE id = ?;", [title, id]);
 
-			fetchTodos(tx);
-		});
-	};
+// 			fetchTodos(tx);
+// 		});
+// 	};
 
-	const deleteTodo = (db, id) => {
-		db.transaction((tx) => {
-			tx.executeSql("DELETE FROM todos WHERE id = ?;", [id]);
+// 	const deleteTodo = (db, id) => {
+// 		db.transaction((tx) => {
+// 			tx.executeSql("DELETE FROM todos WHERE id = ?;", [id]);
 
-			fetchTodos(tx);
-		});
-	};
+// 			fetchTodos(tx);
+// 		});
+// 	};
 
-	return {
-		todos,
-		getTodos,
-		addTodo,
-		updateTodo,
-		deleteTodo
-	};
-}
+// 	return {
+// 		todos,
+// 		getTodos,
+// 		addTodo,
+// 		updateTodo,
+// 		deleteTodo
+// 	};
+// }
