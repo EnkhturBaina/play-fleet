e118d64d27d249b0b807bd49a8d17bd63883dc8f
enkhturBaina <eegii.111.gg@gmail.com>
2025-03-10 11:06:51 +0800

    zaswar

--- src/screens/Notification/NotificationScreen.js
+++ src/screens/Notification/NotificationScreen.js
@@ -1,17 +1,40 @@
-import { Platform, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React, { useContext } from "react";
+import {
+	ActivityIndicator,
+	FlatList,
+	Platform,
+	RefreshControl,
+	StatusBar,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View
+} from "react-native";
+import React, { useContext, useEffect, useState } from "react";
 import Constants from "expo-constants";
 import HeaderUser from "../../components/HeaderUser";
 import { Icon } from "@rneui/base";
 import axios from "axios";
-import { SERVER_URL } from "../../constant";
+import { MAIN_BORDER_RADIUS, MAIN_COLOR, SERVER_URL, TEXT_COLOR_GRAY } from "../../constant";
 import MainContext from "../../contexts/MainContext";
+import Empty from "../../components/Empty";
 
 const NotificationScreen = (props) => {
 	const state = useContext(MainContext);
 
 	const [notificationData, setNotificationData] = useState(null);
 	const [loadingNotification, setLoadingNotification] = useState(false);
+	const [refreshing, setRefreshing] = useState(false);
+
+	const wait = (timeout) => {
+		return new Promise((resolve) => setTimeout(resolve, timeout));
+	};
+
+	const onRefresh = () => {
+		setRefreshing(true);
+		getNotifications();
+		wait(1000).then(() => setRefreshing(false));
+	};
+
 	const getNotifications = async (inspectionId) => {
 		setNotificationData(null);
 		setLoadingNotification(true);
@@ -19,9 +42,7 @@ const NotificationScreen = (props) => {
 			await axios
 				.post(
 					`${SERVER_URL}/mobile/notification/list`,
-					{
-						id: inspectionId
-					},
+					{},
 					{
 						headers: {
 							"Content-Type": "application/json",
@@ -30,20 +51,45 @@ const NotificationScreen = (props) => {
 					}
 				)
 				.then(function (response) {
-					// console.log("report get Inspections response", JSON.stringify(response.data));
+					// console.log("report get Notifications response", JSON.stringify(response.data));
 					if (response.data?.Type == 0) {
-						notificationData(response.data?.Extra);
+						setNotificationData(response.data?.Extra);
 					}
 				})
 				.catch(function (error) {
-					console.log("error report get Inspections", error.response.data);
+					console.log("error report get Notifications", error);
 				})
 				.finally(() => {
 					setLoadingNotification(false);
 				});
 		} catch (error) {
-			console.log("CATCH get Inspections", error);
+			console.log("CATCH get Notifications", error);
 		}
+	};
+
+	useEffect(() => {
+		getNotifications();
+	}, []);
+
+	const renderItem = ({ item }) => {
+		return (
+			<View
+				style={{
+					borderBottomWidth: 1,
+					borderBottomColor: TEXT_COLOR_GRAY,
+					padding: 10,
+					flexDirection: "column"
+				}}
+			>
+				<Text style={{ fontWeight: "600" }}>{item?.notification?.Title}</Text>
+				<Text>{item?.notification?.Message}</Text>
+				<View style={{ flexDirection: "row", alignItems: "center" }}>
+					<Text>{item?.type?.Name}</Text>
+					<Text>{item.created_at}</Text>
+					<Text>{item.state?.Name}</Text>
+				</View>
+			</View>
+		);
 	};
 
 	return (
@@ -73,7 +119,21 @@ const NotificationScreen = (props) => {
 				<Icon name="chevron-left" type="feather" size={25} color="#fff" />
 				<Text style={{ color: "#fff", fontSize: 18, marginLeft: 10 }}>Буцах</Text>
 			</TouchableOpacity>
-			<Text>NotificationScreen</Text>
+			{loadingNotification ? (
+				<ActivityIndicator color={MAIN_COLOR} style={{ flex: 1 }} size={"large"} />
+			) : (
+				<FlatList
+					contentContainerStyle={{
+						flexGrow: 1
+					}}
+					showsVerticalScrollIndicator={false}
+					data={notificationData}
+					renderItem={renderItem}
+					keyExtractor={(item, index) => index.toString()}
+					ListEmptyComponent={<Empty text="Мэдээлэл олдсонгүй." />}
+					refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} tintColor={MAIN_COLOR} />}
+				/>
+			)}
 		</View>
 	);
 };
